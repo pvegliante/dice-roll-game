1c23fe0d3961a4737deadaef3eca741c3db41232
Peter Vegliante <veglianteps2@gmail.com>
2017-01-11 21:23:10 -0500

    completed the app dice-roll-game

---
+++ js/main.js
@@ -0,0 +1,77 @@
+var $dieAndNum = $('#die, #roll');
+var $die = $('#die');
+var $roll = $('#roll');
+var $hillary = $('#player1');
+var $trump = $('#player2');
+var hillaryPos = 0;
+var trumpPos = 0;
+var tiles = {};
+var $winnerCircle = $('#winner-circle')
+var gameOver = false;
+
+
+for (var i = 1; i <= 10; i++) {
+    tiles[i] = $('#tile' + i);
+}
+
+var turn;
+if (Math.random() > 0.5) {
+    turn = 'player1';
+} else {
+    turn = 'player2';
+}
+
+$dieAndNum.click(function() {
+    if (gameOver === true) return false;
+
+    $die.attr('src', './img/Dodecahedron3.gif');
+
+
+    var num = Math.floor(Math.random() * 10 + 1);
+
+    $roll.html(num);
+
+    var $playerToMove;
+    var playerTile;
+    if (turn === 'player1') {
+        $playerToMove = $hillary;
+        turn = 'player2';
+        hillaryPos += num;
+        playerTile = hillaryPos;
+    } else {
+        $playerToMove = $trump;
+        turn = 'player1';
+        trumpPos += num;
+        playerTile = trumpPos;
+    }
+
+    if (trumpPos > 10 || hillaryPos > 10) {
+        onGameOver($playerToMove);
+        return false;
+    }
+
+    var tilePos = tiles[playerTile][0].getBoundingClientRect();
+
+    $playerToMove.css({
+        'top': tilePos.top,
+        'bottom': tilePos.bottom,
+        'left': tilePos.left,
+        'right': tilePos.right,
+    });
+});
+
+
+function onGameOver($winner) {
+    gameOver = true;
+    var tilePos = $winnerCircle[0].getBoundingClientRect();
+    $winner.css({
+        'top': tilePos.top,
+        // 'bottom': tilePos.bottom + 32,
+        'left': tilePos.left,
+        // 'right': tilePos.right + 32,
+    }).addClass('winner-style');
+    setTimeout(function() {
+      alert('Winner!');
+    }, 400);
+    console.log($winner);
+};
